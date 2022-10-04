bf1ece7a07b4cbee5e9ffae15c1724a677fb54bf
Jules Roussel <k20074057@kcl.ac.uk>
2022-10-04 15:41:11 +0100

    All tests passed frequencies ex

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,12 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    counter = 0
+    string = ""
+    for item in items:
+        string = str(item)
+        items[counter] = string
+        counter += 1
+    for item in items:
+        frequencies[item] = items.count(item)
     return frequencies
